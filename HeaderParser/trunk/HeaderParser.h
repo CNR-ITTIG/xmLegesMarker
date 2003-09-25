73f36c22603408caca071284cf5e36ea2355d541
Mirco Taddei <mirco.taddei@ittig.cnr.it>
2003-09-25 13:04:39 +0000

    Initial revision
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesMarker@41 2bb9decf-be17-0410-9831-bbedd0e03ce7

---
+++ HeaderParser/trunk/HeaderParser.h
@@ -0,0 +1,44 @@
+#ifndef HEADERPARSER_H
+#define HEADERPARSER_H
+
+#include "../HMM/HMM.h"
+#include "../FeatureExtractor/TextSequenceFeatureExtractor.h"
+
+class HeaderParser
+{
+  HMM header_model;
+  HMM footer_model;
+  TextSequenceFeatureExtractor header_extractor;
+  TextSequenceFeatureExtractor footer_extractor;
+  std::hash_map<int,int> header_tags;
+  std::hash_map<int,int> footer_tags;
+
+ public:
+  HeaderParser(const char * header_model_file,
+		  const char * footer_model_file,
+		  const char * header_extractor_model_file,
+		  const char * header_extractor_config_file,
+		  const char * footer_extractor_model_file,
+		  const char * footer_extractor_config_file,
+		  const char * parser_config_file);
+  void parseHeader(const char * header);
+  void parseFooter(const char * footer, int offset); 
+  void parseHeader(std::istream& in); 
+  void parseFooter(std::istream& in, int offset); 
+  static const char * tagName(int tagvalue);
+
+ protected:
+  void saveTag(int tagvalue,
+	       int start,
+	       int end,
+	       const std::string& buffer) const;
+  void parse(const char * buffer, 
+	     int offset, 
+	     const HMM& model, 
+	     const std::hash_map<int,int>& tags,
+	     TextSequenceFeatureExtractor& extractor);
+  void init(std::istream& in);
+
+};
+
+#endif
