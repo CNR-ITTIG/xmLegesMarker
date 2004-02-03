dae78133ef5cf6fbac95cdc815e30f1018467420
Andrea <andrea@ittig.cnr.it>
2004-02-03 09:34:48 +0000

    modificati default ( accapo -> "\n")
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesMarker@74 2bb9decf-be17-0410-9831-bbedd0e03ce7

--- HeaderParser/trunk/loadDefaults.sh
+++ HeaderParser/trunk/loadDefaults.sh
@@ -3,5 +3,5 @@ rm -f Default.h
 
 for x in ` ls -1 Models/ | grep CVS -v`
 do
-    echo "const char * ${x}_default=\"`cat Models/$x`\";" >> Default.h  
+    echo "const char * ${x}_default=\"`cat Models/$x | tr '\n' '#' | sed s/#/\\\\\\\\n/g`\";" >> Default.h  
 done
