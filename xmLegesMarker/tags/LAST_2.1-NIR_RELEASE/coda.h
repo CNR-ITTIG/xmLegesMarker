e155ef032c6cd1eb49b96149d2cfb6e5494f5429
Lorenzo Bacci <lorenzo.bacci@icloud.com>
2007-01-16 18:21:52 +0000

    Ultima versione compatibile con DTD NIR 2.1
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesMarker@152 2bb9decf-be17-0410-9831-bbedd0e03ce7

---
+++ xmLegesMarker/tags/LAST_2.1-NIR_RELEASE/coda.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+* Project:	xmLeges
+* Module:	Marker
+* File:		coda.h
+* Copyright:	ITTIG/CNR - Firenze - Italy (http://www.ittig.cnr.it)
+* Licence:	GNU/GPL (http://www.gnu.org/licenses/gpl.html)
+* Authors:	Mirco Taddei (m.taddei@ittig.cnr.it)
+* 			Lorenzo Bacci (lorenzobacci@gmail.com)
+******************************************************************************/
+#ifndef CODA_H
+#define CODA_H
+
+#include <stdio.h>
+int coda( int pnotes,xmlNodePtr ptipodoc, xmlNodePtr pCorpo,xmlNodePtr pmeta,
+				xmlNodePtr pdescrittori,xmlNodePtr pformulafinale,xmlNodePtr pconclusione,int tdoc);
+#endif
---
+++ xmLegesMarker/tags/LAST_2.1-NIR_RELEASE/dom.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+* Project:	xmLeges
+* Module:	Marker
+* File:		dom.h
+* Copyright:	ITTIG/CNR - Firenze - Italy (http://www.ittig.cnr.it)
+* Licence:	GNU/GPL (http://www.gnu.org/licenses/gpl.html)
+* Authors:	Mirco Taddei (m.taddei@ittig.cnr.it)
+* 			Lorenzo Bacci (lorenzobacci@gmail.com)
+******************************************************************************/
+#ifndef __DOM_H__
+#define __DOM_H__
+
+
+#define ATTRIB_ID	"id"
+#define ATTRIB_ID_SEP	"-"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	#include <libxml/tree.h>
+	#include <stdio.h>
+	#include "tag.h"
+	#include "util.h"
+	#include "config.h"
+
+	//Funzioni di Azione sul vettore "State"
+	void		domClearStateFrom(tagTipo ptag);
+	void		domSetCurrentTagState(tagTipo ptag,xmlNodePtr pnode);
+
+	void		domSetID(tagTipo ptag,int numConv,int latConv);
+	void		domSetID2(tagTipo ptag,char *pnomeattr,int pnumConv,int platConv);
+	void		domSetIDtoNode(xmlNodePtr	pnodo, tagTipo ptag,int pnumConv,int platConv,char *pnomeattr);
+	void		domSetIDLettera(char *current_lettera,int latConv);
+	
+	char		*domExtractStringBeforeIndex(int pindex);
+	void		domAppendTextToLastNode(int pIndex);
+
+    void		domInit(tagTipo tRoot,xmlNodePtr	pRoot,  char *ptext);
+	void		domClose(void);
+
+	void		domTagAdd3(tagTipo ptag,int pindex,int plen,int pnumConv,int platConv);
+	xmlNodePtr	domTagOpen(tagTipo ptag,int pindex,int plen);
+	void		domTagInsertEmpty(tagTipo tparent,tagTipo tcur,int pindex);
+	void		domTagClose(tagTipo t);
+	void		domTagCloseFrom(tagTipo t);
+
+	void		domAttributeIDUpdate(xmlNodePtr node,char *pParentID, char * pAnnessoParentID);
+	xmlNodePtr	domGetLastTag(tagTipo ptag);
+	void 		domAddRango(xmlNodePtr node, char *t);
+	xmlNodePtr	domGetFirstNode(xmlNodePtr node);
+	void domAddSequenceWarning(tagTipo ptag, int num, int lat);
+	int domPIAdded(xmlNodePtr node);
+	void domInitStates();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
